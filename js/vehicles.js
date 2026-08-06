76d335dc35816753bdf832bec453b3174d31b65d
ivosisevic00-del <ivo.sisevic00@gmail.com>
2026-08-07 00:24:16 +0200

    2

--- js/vehicles.js
+++ js/vehicles.js
@@ -1,3 +1,158 @@
+let editingVehicle = -1;
+
 function initVehicles() {
-    // Modul vozila dolazi uskoro
+
+    renderVehicles();
+
+    document
+        .getElementById("newVehicleBtn")
+        ?.addEventListener("click", openVehicleModal);
+
+    document
+        .getElementById("saveVehicle")
+        ?.addEventListener("click", saveVehicle);
+
+    document
+        .getElementById("closeVehicle")
+        ?.addEventListener("click", () => closeModal("vehicleModal"));
+
+}
+
+function renderVehicles() {
+
+    const tbody = document.getElementById("vehiclesBody");
+
+    if (!tbody) return;
+
+    tbody.innerHTML = "";
+
+    DB.vehicles.forEach((vehicle, index) => {
+
+        tbody.innerHTML += `
+
+        <tr>
+
+            <td>${vehicle.brand}</td>
+
+            <td>${vehicle.model}</td>
+
+            <td>${vehicle.year}</td>
+
+            <td>${vehicle.plate}</td>
+
+            <td>
+
+                <button class="editBtn"
+                    onclick="editVehicle(${index})">
+
+                    <i class="fa-solid fa-pen"></i>
+
+                </button>
+
+                <button class="deleteBtn"
+                    onclick="deleteVehicle(${index})">
+
+                    <i class="fa-solid fa-trash"></i>
+
+                </button>
+
+            </td>
+
+        </tr>
+
+        `;
+
+    });
+
+}
+
+function openVehicleModal() {
+
+    editingVehicle = -1;
+
+    vehicleBrand.value = "";
+    vehicleModel.value = "";
+    vehicleYear.value = "";
+    vehiclePlate.value = "";
+
+    openModal("vehicleModal");
+
+}
+
+function saveVehicle() {
+
+    const vehicle = {
+
+        brand: vehicleBrand.value,
+
+        model: vehicleModel.value,
+
+        year: vehicleYear.value,
+
+        plate: vehiclePlate.value
+
+    };
+
+    if (vehicle.brand.trim() === "") {
+
+        alert("Unesi marku.");
+
+        return;
+
+    }
+
+    if (editingVehicle === -1) {
+
+        DB.vehicles.push(vehicle);
+
+        toast("Dodano vozilo");
+
+    } else {
+
+        DB.vehicles[editingVehicle] = vehicle;
+
+        toast("Ažurirano vozilo");
+
+    }
+
+    saveStorage();
+
+    renderVehicles();
+
+    updateDashboard();
+
+    closeModal("vehicleModal");
+
+}
+
+function editVehicle(index) {
+
+    editingVehicle = index;
+
+    const v = DB.vehicles[index];
+
+    vehicleBrand.value = v.brand;
+    vehicleModel.value = v.model;
+    vehicleYear.value = v.year;
+    vehiclePlate.value = v.plate;
+
+    openModal("vehicleModal");
+
+}
+
+function deleteVehicle(index) {
+
+    if (!confirm("Obrisati vozilo?"))
+        return;
+
+    DB.vehicles.splice(index, 1);
+
+    saveStorage();
+
+    renderVehicles();
+
+    updateDashboard();
+
+    toast("Vozilo obrisano");
+
 }
